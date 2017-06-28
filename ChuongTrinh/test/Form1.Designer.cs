3b1079ed70acdc94713bbc099aee16ea8d17fcec
congtuuit <congtuuit@gmail.com>
2017-06-28 21:48:12 +0700

    bt1

---
+++ ChuongTrinh/test/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.btLoad = new System.Windows.Forms.Button();
+            this.openFileDialog2 = new System.Windows.Forms.OpenFileDialog();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // btLoad
+            // 
+            this.btLoad.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btLoad.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.btLoad.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btLoad.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btLoad.Location = new System.Drawing.Point(29, 153);
+            this.btLoad.Name = "btLoad";
+            this.btLoad.Size = new System.Drawing.Size(90, 43);
+            this.btLoad.TabIndex = 0;
+            this.btLoad.Text = "Load file...";
+            this.btLoad.UseVisualStyleBackColor = false;
+            this.btLoad.Click += new System.EventHandler(this.btLoad_Click);
+            // 
+            // openFileDialog2
+            // 
+            this.openFileDialog2.FileName = "openFileDialog2";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 70;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(261, 124);
+            this.panel1.TabIndex = 1;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 107);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "label2";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 223);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.Location = new System.Drawing.Point(159, 153);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(90, 43);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Exit";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::test.Properties.Resources.cloud_storage_hp_100571955_primary_idge;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(285, 208);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.btLoad);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Count the words in the text.";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button btLoad;
+        private System.Windows.Forms.OpenFileDialog openFileDialog2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ ChuongTrinh/test/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Collections.Generic;
+using System.Collections;
+using System.Text;
+
+namespace test
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void btLoad_Click(object sender, EventArgs e)
+        {
+            openFileDialog2.ShowDialog();
+            StreamReader text = new StreamReader(openFileDialog2.FileName);
+            string txt = text.ReadToEnd();
+            string[] a = txt.Split('\n');
+           
+           MessageBox.Show("Chọn file Văn bản!");
+           openFileDialog1.ShowDialog();
+           StreamReader sr = new StreamReader(openFileDialog1.FileName);
+           string b = sr.ReadToEnd();
+           /* Xu ly van ban*/
+           #region
+           b = b.Replace(",", "");
+           //b = b.Replace(". ", " ");
+           b = b.Replace(". ", " ");
+           b = b.Replace(".", " ");           
+           b = b.Replace("...", "");             
+           b = b.Replace("'", "");
+           b = b.Replace("!", "");
+           b = b.Replace("(", "");
+           b = b.Replace(")", "");
+           b = b.Replace(":", "");
+           b = b.Replace(";", "");
+           b = b.Replace("?", "");
+           b = b.Replace("=", "");
+           b = b.Replace("*", "");
+           b = b.Replace("/", "");
+           b = b.Replace("+", "");
+           b = b.Replace("-", "");
+           b = b.Replace("_", "");
+           b = b.Replace("@", "");
+           b = b.Replace("{", "");
+           b = b.Replace("}", "");
+           b = b.Replace("$", "");
+           
+           b = b.ToUpper(); //In hoa VB
+          // b = b.Replace(" ", "");
+           #endregion
+           
+           string[] vb = b.Split(' ');
+           int dem = 0;
+         MessageBox.Show("Bắt đầu kiểm tra...");
+
+          float sum=0;
+          for (int i = 0; i < a.Length; i++)
+          {
+              int flash = 0;
+              for (int j = 0; j < vb.Length; j++)
+                      if (a[i] == vb[j])
+                      {
+                          flash++;
+                      }
+              if (flash >= 1)
+                  sum++;
+          }
+          string x=Convert.ToString(sum);
+          float KQ = (sum / 7000)*100; 
+          string kqsum = Convert.ToString(KQ);
+          MessageBox.Show(x, "Số tiếng trong danh sách A xuất hiện trong file B");
+          MessageBox.Show(kqsum, "% xuất hiện.");
+          txt.Clone();
+          sr.Close();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+           
+            label2.Location = new Point(label2.Location.X, label2.Location.Y - 1);
+            if (label2.Location.Y + label2.Height < 0)
+                label2.Location = new Point(label2.Location.X, panel1.Height);
+
+        }
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label2.Text = "Bước 1: Chọn đường dẫn đến file A chứa danh sách\ncác tiếng.\nBước 2: Chọn đường dẫn đến file văn bản B.\n Xem kết quả!";
+            timer1.Enabled = true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+       
+    }
+}
